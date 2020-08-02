ef55148bf3d6e844aad3bd3b2d158f6160aad68f
nikolajgriskin <din.nick@bk.ru>
2020-08-02 19:28:19 +0500

    file-sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,31 @@
 const
     gulp = require('gulp'),
     ts = require('gulp-typescript'),
-    log = require('fancy-log'),
-    spawn = require('child_process').spawn;
-
-let node;
+    fileSync = require('gulp-file-sync');
 
 const tsProject = ts.createProject('tsconfig.json');
 
 const sourcePath = 'src';
-const sourceMask = `${sourcePath}/*.ts`;
-
 const distPath = 'dist';
 
 gulp.task('build', function () {
-    return gulp.src(sourceMask)
+    return gulp.src(`${sourcePath}/*.ts`)
         .pipe(tsProject())
         .pipe(gulp.dest(distPath));
 });
 
-gulp.task('run', function () {
-    if (node) node.kill();
-    node = spawn('node', [`${distPath}/index.js`], { stdio: 'inherit' });
-    node.on('close', function (code) {
-        if (code) {
-            log(`child process exited with code ${code}`);
-        }
+gulp.task('sync-files', function () {
+    fileSync(sourcePath, distPath, {
+        ignore: /^.+\.ts$/i,
     });
 
     return Promise.resolve();
 });
 
 gulp.task('watch', function () {
-    gulp.watch(`${sourcePath}/**/*`, gulp.series('build', /*'sass', 'html', */'run'));
+    gulp.watch(`${sourcePath}/**/*`, gulp.series('sync-files', 'build'));
 
     return Promise.resolve();
 });
 
-gulp.task('serve', gulp.series('build', /*'sass', 'html', */'run', 'watch'));
-
-process.on('exit', function () {
-    if (node) node.kill();
-    log('Exit with code 0');
-});
+gulp.task('serve', gulp.series('sync-files', 'build', 'watch'));
