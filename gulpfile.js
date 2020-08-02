510b33ab42e598d22abaf435a44f6a3422491e37
nikolajgriskin <din.nick@bk.ru>
2020-08-02 18:49:41 +0500

    package-lock.json typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,47 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+'use strict';
 
-function bs() {
-    browserSync.init({
-        files: ["src/*"],
-        server: {
-            baseDir: "./src",
-            routes: {
-                '/node_modules': './node_modules'
-            }
-        }
-    });
-}
+const
+    gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    log = require('fancy-log'),
+    spawn = require('child_process').spawn;
 
-gulp.task('browser-sync', function() {
-    bs();
+let node;
+
+const tsProject = ts.createProject('tsconfig.json');
+
+const sourcePath = 'src';
+const sourceMask = `${sourcePath}/*.ts`;
+
+const distPath = 'dist';
+
+gulp.task('build', function () {
+    return gulp.src(sourceMask)
+        .pipe(tsProject())
+        .pipe(gulp.dest(distPath));
 });
 
-gulp.task('default', function () {
-    bs();
+gulp.task('run', function () {
+    if (node) node.kill();
+    node = spawn('node', [`${distPath}/index.js`], { stdio: 'inherit' });
+    node.on('close', function (code) {
+        if (code) {
+            log(`child process exited with code ${code}`);
+        }
+    });
+
+    return Promise.resolve();
+});
+
+gulp.task('watch', function () {
+    gulp.watch(`${sourcePath}/**/*`, gulp.series('build', /*'sass', 'html', */'run'));
+
+    return Promise.resolve();
+});
+
+gulp.task('serve', gulp.series('build', /*'sass', 'html', */'run', 'watch'));
+
+process.on('exit', function () {
+    if (node) node.kill();
+    log('Exit with code 0');
 });
