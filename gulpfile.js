dceb1a502305974c2d885ce9ef379cf0a3427f46
nikolajgriskin <din.nick@bk.ru>
2020-09-15 20:20:11 +0500

    the building is ready.

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,73 @@
 
 const
     gulp = require('gulp'),
-    ts = require('gulp-typescript'),
-    fileSync = require('gulp-file-sync');
-
-const tsProject = ts.createProject('tsconfig.json');
+    fileSync = require('gulp-file-sync'),
+    fs = require('fs'),
+    browserify = require("browserify"),
+    source = require("vinyl-source-stream"),
+    tsify = require("tsify"),
+    browserSync = require('browser-sync').create();
 
 const sourcePath = 'src';
 const distPath = 'dist';
+const nodeModules = 'node_modules';
 
 gulp.task('build', function () {
-    return gulp.src(`${sourcePath}/*.ts`)
-        .pipe(tsProject())
+    return browserify({
+        basedir: ".",
+        debug: true,
+        entries: [`${sourcePath}/main.ts`],
+        cache: {},
+        packageCache: {},
+    }).plugin(tsify)
+        .bundle()
+        .pipe(source("bundle.js"))
         .pipe(gulp.dest(distPath));
 });
 
 gulp.task('sync-files', function () {
-    fileSync(sourcePath, distPath, {
-        ignore: /^.+\.ts$/i,
-    });
+    return new Promise((resolve, reject) => {
+        fileSync(sourcePath, distPath, {
+            ignore: /^.+\.ts$/i,
+        });
 
-    return Promise.resolve();
+        let d3Ready, d3Hierarchy;
+
+        const callback = (err) => {
+            if (err) {
+                reject(err);
+            }
+
+            if (d3Ready && d3Hierarchy) {
+                resolve();
+            }
+        }
+
+        fs.copyFile(`${nodeModules}/d3/dist/d3.min.js`, `${distPath}/d3.min.js`, (err) => {
+            d3Ready = true;
+            callback(err);
+        });
+
+        fs.copyFile(`${nodeModules}/d3-hierarchy/dist/d3-hierarchy.min.js`, `${distPath}/d3-hierarchy.min.js`, (err) => {
+            d3Hierarchy = true;
+            callback(err);
+        });
+    });
+});
+
+gulp.task('reload-browser', function (done) {
+    browserSync.reload();
+    done();
 });
 
 gulp.task('watch', function () {
-    gulp.watch(`${sourcePath}/**/*`, gulp.series('sync-files', 'build'));
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
 
-    return Promise.resolve();
+    return gulp.watch(`${sourcePath}/**/*`, gulp.series('sync-files', 'build', 'reload-browser'));
 });
 
 gulp.task('serve', gulp.series('sync-files', 'build', 'watch'));
